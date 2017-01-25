ab30fe281dc74ce446678f90c843454fe043721a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-24 17:51:33 -0800

    Added student profile and ability to see projects on student profile

--- hackbright-web.py
+++ hackbright-web.py
@@ -9,9 +9,58 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    projects = hackbright.get_grades_by_github(github)
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github,
+                           projects=projects)
+
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/add-student")
+def add_student():
+    """Add a student."""
+
+    return render_template("create_new_student.html")
+
+
+@app.route("/new-student-success", methods=["POST"])
+def new_student_success():
+    """Return success message after user adds new student."""
+
+
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+    github = request.form.get("github")
+
+    hackbright.make_new_student(fname, lname, github)
+
+    return render_template("new_student_confirmation.html",
+                           fname=fname,
+                           lname=lname,
+                           github=github)
+
+
+@app.route("/project")
+def see_project():
+    """Display information about a student project."""
+
+    title = request.args.get("title")
+    project_info = hackbright.get_project_by_title(title)
+
+    return render_template("display_project.html", project_info=project_info)
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
